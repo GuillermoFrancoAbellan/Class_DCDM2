8fcfe217ba8ca2519bd28c5d77c890a989a79d69
GuillermoFrancoAbellan <59618978+GuillermoFrancoAbellan@users.noreply.github.com>
2020-01-08 20:29:14 +0100

    Add files via upload

--- include/background.h
+++ include/background.h
@@ -84,11 +84,15 @@ struct background
 
   double Omega0_dcdmdr; /**< \f$ \Omega_{0 dcdm}+\Omega_{0 dr} \f$: decaying cold dark matter (dcdm) decaying to dark radiation (dr) */
 
+  double Omega0_dcdm2dr2wdm2; /**< \f$ GFA: \Omega_{0 dcdm2}+\Omega_{0 dr2}+\Omega_{0 wdm2} \f$: decaying cold dark matter (dcdm2) decaying to dark radiation (dr2) and warm dark matter (wdm2) */
+
   double Gamma_dcdm; /**< \f$ \Gamma_{dcdm} \f$: decay constant for decaying cold dark matter */
 
   double Gamma_dcdm2; /**< GFA: decay constant for two-body decay of dark matter*/
 
   double Omega_ini_dcdm;    /**< \f$ \Omega_{ini,dcdm} \f$: rescaled initial value for dcdm density (see 1407.2418 for definitions) */
+
+  double Omega_ini_dcdm2;    /**< GFA: \f$ \Omega_{ini,dcdm2} \f$: rescaled initial value for dcdm density (two-body decay) */
 
   double Omega0_scf;        /**< \f$ \Omega_{0 scf} \f$: scalar field */
   short attractor_ic_scf;   /**< whether the scalar field has attractor initial conditions */
@@ -151,6 +155,9 @@ struct background
   double Neff; /**< so-called "effective neutrino number", computed at earliest time in interpolation table */
   double Omega0_dcdm; /**< \f$ \Omega_{0 dcdm} \f$: decaying cold dark matter */
   double Omega0_dr; /**< \f$ \Omega_{0 dr} \f$: decay radiation */
+  double Omega0_dcdm2; /**< GFA: \f$ \Omega_{0 dcdm2} \f$: decaying cold dark matter */
+  double Omega0_dr2; /**< GFA: \f$ \Omega_{0 dr2} \f$: decay dark radiation */
+  double Omega0_wdm2; /**< GFA: \f$ \Omega_{0 wdm2} \f$: decay warm dark matter */
   double Omega0_m;  /**< total non-relativistic matter today */
   double Omega0_r;  /**< total ultra-relativistic radiation today */
   double Omega0_de; /**< total dark energy density today, currently defined as 1 - Omega0_m - Omega0_r - Omega0_k */
