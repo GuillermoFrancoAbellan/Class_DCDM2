087bb3df849f59c5297a5cff9cef6ec5e88ac0fa
PoulinV <vivian.poulin@umontpellier.fr>
2020-02-11 18:26:08 +0100

    add rs_rec to classy + debug classy but only for python2

--- python/classy.pyx
+++ python/classy.pyx
@@ -1027,7 +1027,10 @@ cdef class Class:
 
     def h(self):
         return self.ba.h
-
+    
+    def rs_rec(self):
+        return self.th.rs_rec
+ 
     def n_s(self):
         return self.pm.n_s
 
